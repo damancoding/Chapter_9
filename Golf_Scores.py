83bbe57610ef918f0780ef3ea7dd82183bc3751e
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-03-27 16:38:57 -0400

    Add files via upload
    Will display the last number inputted as well as brackets with elipses.

---
+++ Golf_Scores.py
@@ -0,0 +1,26 @@
+#Sorted Golf = Selection sort
+#****************************************
+golfScore = []
+def golf_Course():
+    for i in range(10):
+        scores = int(input("Please enter 10 golf scores here, line after line: "))
+        golfScore.append(scores)
+
+    startScan = 0
+    while startScan < len(golfScore) - 1:
+        min_index = startScan
+        min_value = golfScore
+        index = startScan + 1
+        while index < len(golfScore):
+            if golfScore[index] < len(min_value):
+                min = golfScore[index]
+                min_index = index
+            else:
+                index = index + 1
+            index = index + 1
+        golfScore[min_index] = golfScore[startScan]
+        golfScore[startScan] = min_value
+        startScan = startScan + 1
+
+golf_Course()
+print(golfScore)
