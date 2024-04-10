cad159b49e105a4b4ba13c246c1c38c22b32d210
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-04-09 20:56:14 -0400

    Create golf_scores.py

---
+++ Homework/golf_scores.py
@@ -0,0 +1,28 @@
+# Amanda M.
+# 3/27/24
+# Golf Scores Chapter 9 Exercise 1
+golfScore = []
+def golf_Course():
+    for i in range(10):
+        scores = int(input("Please enter 10 golf scores here, line after line: "))
+        golfScore.sort()
+        golfScore.append(scores)
+
+    startScan = 0
+    while startScan < len(golfScore) - 1:
+        min_index = startScan
+        min_value = golfScore
+        index = startScan + 1
+        while index < len(golfScore):
+            if golfScore[index] < len(min_value):
+                min = golfScore[index]
+                min_index = index
+            else:
+                index = index + 1
+            index = index + 1
+        golfScore[min_index] = golfScore[startScan]
+        golfScore[startScan] = min_value
+        startScan = startScan + 1
+
+golf_Course()
+print(golfScore)
